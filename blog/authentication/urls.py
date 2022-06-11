e61cabce2bc49788ec148ea5f6a4c2fe1381e24c
Nimish Medatwal <medatwalnimish@gmail.com>
2022-06-12 04:57:11 +0530

    made login, signup pages

---
+++ blog/authentication/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path("register", views.register, name="register"),
+    path("",views.login_user),
+    path("login_user", views.login_user, name="login_user"),
+    path("logout_user", views.logout_user, name="logout_user"),
+    path("home", views.home, name="home")
+]
